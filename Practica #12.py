f460c19716a418d387a2e876d3239ad8e510c95a
RAM-2320 <Rogerarmandomelomontero@gmail.com>
2024-05-30 19:47:40 -0400

    Add files via upload

---
+++ Practica #12.py
@@ -0,0 +1,14 @@
+valor_1 = int(input("Ingrese el primer valor: "))
+valor_2 = int(input("Ingrese el segundo valor:"))
+
+diferncia = abs(valor_1 - valor_2)
+
+if diferncia % 2 == 0:
+
+  print ("La suma de los valores:", valor_1 + valor_2)
+elif diferncia < 10 and diferncia in [2,3,5,7]:
+  print("El producto es: ", valor_1 * valor_2)
+elif diferncia % 10 == 4:
+  print ("Los digitos son: ", valor_1, valor_2)
+else:
+  print("Los valores dados no cumplen ni una condicion :(")
